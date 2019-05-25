96c92289a895ae388e374c3bdda867faa8393f0a
Tigur <SirResnick2@gmail.com>
2019-05-25 15:42:49 +0200

    file reading problem

---
+++ neh_BoAiR.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+//#include <conio.h>
+#include <fstream>
+#include <stdio.h>
+#include <algorithm>
+//#include <Windows.h>
+#include <queue>
+
+using namespace std;
+
+int n;  //ilosc zadan
+int m;  //ilosc maszyn
+
+struct Zadanie{
+
+	int job_nr;
+	int *machine_time;
+	int Cmax;
+
+	void oblicz_cmax();
+};
+
+struct Machine: public Zadanie {
+
+	int *machine_nr;
+	int current_time;
+	int end_time;
+
+};
+
+void Zadanie::oblicz_cmax()
+{	
+	Cmax = 0;
+	for (int i = 0; i < m; i++) {
+		Cmax = Cmax + machine_time[i];
+	}
+}
+
+struct Cmax_up	//przeciazenie operatora porownania zadan
+{
+
+	bool operator()(Machine& a, Machine& b)
+	{
+		return a.Cmax < b.Cmax;
+	}
+};
+
+priority_queue<Machine, vector<Machine>, Cmax_up> kolejka;
+priority_queue<Machine, vector<Machine>, Cmax_up> temp_queue;
+
+Machine * tab;
+
+istream& operator >> (istream& wejscie, Machine& zadanie)	//przeciazenie operatora wejscia
+{
+	for (int i = 0; i < m; i++) {
+		wejscie >> zadanie.machine_time[i];
+	}
+	return wejscie;
+}
+
+ostream& operator << (ostream& wyjscie, Machine& zadanie)  //przeciazenie operatora wyjscia
+{
+		cout << zadanie.job_nr << " | ";
+		for (int i = 0; i < m; i++) {
+		cout << zadanie.machine_time[i] << " ";
+		}
+		cout << "| " << zadanie.Cmax << endl;
+	return wyjscie;
+}
+
+void wczytaj()
+{
+	fstream plik;
+
+	plik.open("NEH1.dat", ios::in);
+
+	if (plik.good() == false)
+	{
+		cout << "Nie mozna otworzyc pliku!" << endl;
+	};
+
+	plik >> n;   // Wczytanie ilosci wierszy
+	plik >> m;   // Wczytanie ilosci maszyn
+
+	Machine k;
+	tab = new Machine[n];
+	k.machine_time = new int[m];
+	/*Machine * tab = new Machine[n];
+
+	for (int i = 0; i < n; i++) {
+		tab[i].job_nr = i + 1;
+		plik >> tab[i];
+	}*/
+	for (int i = 0; i < n; i++)   //Wczytanie wartosci z pliku do kolejki
+	{
+		k.job_nr = i + 1;
+		plik >> k;
+		/*for (int i = 0; i < m; i++) {
+			k.machine_nr[i] = i + 1;
+		}*/
+		k.oblicz_cmax();
+		cout << k;
+		tab[i] = k;
+		kolejka.push(k);
+	}
+	plik.close();
+	//delete[] k.machine_time;
+	//delete[] tab;
+};
+
+void order() {	//wyswietlanie zadan w kolejnosci po Cmax
+
+	/*for (int i = 0; i < n; i++)
+		cout << tab[i];*/
+
+	while (kolejka.size() > 0) {
+		Machine nowe = kolejka.top();
+		kolejka.pop();
+		temp_queue.push(nowe);
+		cout << nowe;
+	}
+
+	while (temp_queue.size() > 0) {
+		Machine nowe = temp_queue.top();
+		temp_queue.pop();
+		kolejka.push(nowe);
+	}
+}
+
+/*void Neh() {
+
+	Machine * tab_seq = new Machine[n];
+	Machine * tab = new Machine[n];
+	int iteracja = 0;
+
+	while (kolejka.size() > 0) {
+		Machine nowe = kolejka.top();
+		kolejka.pop();
+		tab[iteracja] = nowe;
+		tab[iteracja++];
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << tab[i];
+	}
+
+	int amount = 4;
+	for (int i = 0; i < amount; i++) {
+		tab_seq[i] = tab[i];
+		cout << tab_seq[i];
+	}
+	delete tab_seq;
+	delete tab;
+	
+
+	}*/
+
+int main() {
+
+	Machine aa;
+	wczytaj();
+	cout << endl;
+	order();
+	cout << endl;
+	kolejka.pop();
+	aa = kolejka.top();
+	cout << aa;
+	//ss = kolejka.top();
+	//Neh();
+	cout << endl;
+	//cout << ss;
+	getchar();
+	system("CLS");
+}
