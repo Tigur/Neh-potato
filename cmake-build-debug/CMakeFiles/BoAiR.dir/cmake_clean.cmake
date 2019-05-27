bcd9e135fa54422d343ec83de741ac0adb956125
Tigur <SirResnick2@gmail.com>
2019-05-27 21:18:10 +0200

    new debug funtions and Clion compatibility

---
+++ cmake-build-debug/CMakeFiles/BoAiR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BoAiR.dir/neh_BoAiR.cpp.o"
+  "BoAiR.pdb"
+  "BoAiR"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoAiR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
